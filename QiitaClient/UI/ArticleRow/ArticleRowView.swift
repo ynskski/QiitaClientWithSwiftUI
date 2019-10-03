59cf564b43c497d13904c9169dead543b97631a6
Yunosuke <qmkns246@gmail.com>
2019-10-03 20:04:25 +0900

    ArticleListの１行を表すArticleRowViewを作成

---
+++ QiitaClient/UI/ArticleRow/ArticleRowView.swift
@@ -0,0 +1,33 @@
+//
+//  ArticleRowView.swift
+//  QiitaClient
+//
+//  Created by 堺雄之介 on 2019/10/03.
+//  Copyright © 2019 Yunosuke Sakai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ArticleRowView: View {
+    var article: Article
+    
+    var body: some View {
+        HStack(alignment: .top) {
+            ProfileImageView(imageURL: article.user.profileImageURL)
+            
+            VStack(alignment: .leading) {
+                Text(article.title)
+                    .lineLimit(nil)
+                
+                Text("by \(article.user.id)")
+                    .font(.caption)
+            }
+        }
+    }
+}
+
+struct ArticleRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArticleRowView(article: Article(id: "0", title: "title", url: "http://test", user: Article.User(id: "1", name: "name", profileImageURL: "http://profile")))
+    }
+}
