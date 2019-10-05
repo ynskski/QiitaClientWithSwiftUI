7b681cfabd9f521c00b2bba434027883382a7be0
Yunosuke <qmkns246@gmail.com>
2019-10-06 00:51:57 +0900

    ActivityIndicatorを作成

---
+++ QiitaClient/UI/ActivityIndicator/ActivityIndicator.swift
@@ -0,0 +1,27 @@
+//
+//  ActivityIndicator.swift
+//  QiitaClient
+//
+//  Created by 堺雄之介 on 2019/10/06.
+//  Copyright © 2019 Yunosuke Sakai. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+struct ActivityIndicator: UIViewRepresentable {
+    @State var isLoading: Bool
+    let style: UIActivityIndicatorView.Style
+    
+    func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> UIActivityIndicatorView {
+        return UIActivityIndicatorView(style: style)
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicator>) {
+        if isLoading {
+            uiView.startAnimating()
+        } else {
+            uiView.stopAnimating()
+        }
+    }
+}
